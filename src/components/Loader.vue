87b7a7ed0af559c167dd14ab43dd1d274bbac953
Damiano <ghgg585@gmail.com>
2024-04-15 19:27:05 +0200

    Fix loader class rounded to rounded-circle

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -5,7 +5,7 @@ export default{
 </script>
 <template>
     <div class="container d-flex justify-content-center ">
-        <div class="img-container rounded overflow-hidden">
+        <div class="img-container rounded-circle overflow-hidden">
             <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGV4bWx4N3lldWN5ajB6bDN5dXowMjMxaHhwbm1maTY2bzJ0eHdpdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZJPSFNLmADueHvzoZ8/giphy.gif" alt="">
         </div>
     </div>
